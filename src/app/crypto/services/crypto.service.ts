7ffdb70c9859d8116407b2708c3effdf5296e694
Matin KHD <matink714@gmail.com>
2023-03-04 10:58:42 +0330

    create crypto service and save the url in enviornment

--- src/app/crypto/services/crypto.service.ts
+++ src/app/crypto/services/crypto.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {CryptoInterface} from "../types/crypto.interface";
+import {HttpClient} from "@angular/common/http";
+import {cryptoUrl} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getCrypto():Observable<CryptoInterface[]> {
-    return
+    return this.http.get<CryptoInterface[]>(cryptoUrl);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,20 @@ export const environment = {
   production: false
 };
 
+export const cryptoUrl = `
+https://api.coingecko.
+com/api/v3/coins/markets
+?vs_currency=usd&order
+=market_cap_desc&per_page=100&page=
+1&sparkline=false
+`;
+
+
 /*
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * to ignore zone related error stack frames such as `
+zone.run`, `
+zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
