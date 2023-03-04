a738f9287fcfbf2ecc8a8863a06e39e15fc37d74
Matin KHD <matink714@gmail.com>
2023-03-04 11:09:25 +0330

    move some code from crypto component ts to crypto-list component ts

--- src/app/crypto/components/crypto-list/crypto-list.component.ts
+++ src/app/crypto/components/crypto-list/crypto-list.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {CryptoInterface} from "../../types/crypto.interface";
+import {select, Store} from "@ngrx/store";
+import {AppStateInterface} from "../../../types/appState.interface";
+import {cryptoSelector, errorSelector, isLoadingSelector} from "../../store/selectors";
+import * as CryptoActions from "../../store/actions";
 
 @Component({
   selector: 'app-crypto-list',
@@ -6,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./crypto-list.component.scss']
 })
 export class CryptoListComponent {
+  isLoading!: Observable<boolean>;
+  error$!: Observable<string | null>;
+  crypto$!: Observable<CryptoInterface[]>
+
+  constructor(private store: Store<AppStateInterface>) {
+    this.isLoading = this.store.pipe(select(isLoadingSelector));
+    this.crypto$ = this.store.pipe(select(cryptoSelector));
+    this.error$ = this.store.pipe(select(errorSelector))
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(CryptoActions.getCrypto());
+  }
 
 }
--- src/app/crypto/crypto.component.ts
+++ src/app/crypto/crypto.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {AppStateInterface} from "../types/appState.interface";
-import {Observable} from "rxjs";
-import {CryptoInterface} from "./types/crypto.interface";
-import {cryptoSelector, errorSelector, isLoadingSelector} from "./store/selectors";
-import * as CryptoActions from "./store/actions"
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -12,19 +6,5 @@ import * as CryptoActions from "./store/actions"
   templateUrl: './crypto.component.html',
   styleUrls: ['./crypto.component.scss']
 })
-export class CryptoComponent implements OnInit {
-  isLoading!: Observable<boolean>;
-  error$!: Observable<string | null>;
-  crypto$!: Observable<CryptoInterface[]>
-
-  constructor(private store: Store<AppStateInterface>) {
-    this.isLoading = this.store.pipe(select(isLoadingSelector));
-    this.crypto$ = this.store.pipe(select(cryptoSelector));
-    this.error$ = this.store.pipe(select(errorSelector))
-  }
-
-  ngOnInit(): void {
-    this.store.dispatch(CryptoActions.getCrypto());
-  }
-
+export class CryptoComponent {
 }
